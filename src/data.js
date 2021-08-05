286ab6580d9bf19f933a530acdd748b990bdc3f3
sammiearchie77 <chinonsosamuel29@gmail.com>
2021-08-05 18:11:34 +0100

    client side ready

---
+++ src/data.js
@@ -0,0 +1,64 @@
+
+
+export const projects = [
+    {
+        title: "React Reserve",
+        subtitle: "MERN Stack",
+        description:  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: "",
+        link: "https://reactbootcamp.com",
+    },
+    {
+        title: "React Tracks",
+        subtitle: "React and Python",
+        description:  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: "",
+        link: "https://reedbarger.com",
+    },
+    {
+        title: "DevChat",
+        subtitle: "React and Firebase",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: "./project-3.gif",
+        link: "https://jsbootcamp.com",
+      },
+      {
+        title: "Epic Todo App",
+        subtitle: "React Hooks",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: "./project-4.gif",
+        link: "https://pythonbootcamp.com",
+      },
+];
+
+export const skills = [
+    {
+        skills: "React",
+    },
+];
+
+export const testimonials = [
+    {
+        name: "",
+        company: "",
+        quote: "",
+        image: "",
+        phone: "",
+    },
+    {
+        name: "",
+        company: "",
+        quote: "",
+        image: "",
+        phone: "",
+    },
+    {
+        name: "",
+        company: "",
+        quote: "",
+        image: "",
+        phone: "",
+    },
+];
